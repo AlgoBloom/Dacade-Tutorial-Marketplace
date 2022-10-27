590e614b200a344c0ab3d6d4f383be8b7fbb5172
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-27 13:54:51 -0400

    Added new algod client, indexer client, and wallet connection.

--- marketplace/src/utils/constants.js
+++ marketplace/src/utils/constants.js
@@ -1,9 +1,9 @@
 // here we define config for connection to algo network
 
 // import algosdk we need to connect with blockchain
-import algosdk from "algosdk";
+import algosdk from "algosdk"
 // import wallet library
-import MyAlgoConnect from "@randlabs/myalgo-connect";
+import MyAlgoConnect from "@randlabs/myalgo-connect"
 
 // set the configuration for connection to test net
 const config = {
@@ -14,3 +14,10 @@ const config = {
     indexerServer: "https://algoindexer.testnet.algoexplorerapi.io",
     indexerPort: "",
 }
+
+// create new algod client
+export const algodClient = new algosdk.Algodv2(config.algodToken, config.algodServer, config.algodPort)
+// create indexer client
+export const indexerClient = new algosdk.Indexer(config.indexerToken, config.indexerServer, config.indexerPort)
+// create wallet connection
+export const MyAlgoConnect = new MyAlgoConnect()
