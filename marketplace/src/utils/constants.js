a241325d8bc632542120c20897c317025199c679
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-27 13:50:21 -0400

    Added configuration library.

--- marketplace/src/utils/constants.js
+++ marketplace/src/utils/constants.js
@@ -4,3 +4,13 @@
 import algosdk from "algosdk";
 // import wallet library
 import MyAlgoConnect from "@randlabs/myalgo-connect";
+
+// set the configuration for connection to test net
+const config = {
+    algodToken: "",
+    algodServer: "https://node.testnet.algoexplorerapi.io",
+    algodPort: "",
+    indexerToken: "",
+    indexerServer: "https://algoindexer.testnet.algoexplorerapi.io",
+    indexerPort: "",
+}
