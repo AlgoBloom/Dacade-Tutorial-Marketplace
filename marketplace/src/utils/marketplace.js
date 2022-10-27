d186c7c869a31e7ff79296f6fe2077148ac34a7a
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-27 18:16:29 -0400

    Added product constructor to the front end.

--- marketplace/src/utils/marketplace.js
+++ marketplace/src/utils/marketplace.js
@@ -16,4 +16,14 @@ import approvalProgram from "!!raw-loader!../contracts/marketplace_approval.teal
 import clearProgram from "!!raw-loader!../contracts/marketplace_clear.teal"
 import { base64ToUTF8String, utf8ToBase64String } from "./conversions"
 
-
+class Product {
+    constructor(name, image, description, price, sold, appId, owner) {
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.sold = sold;
+        this.appId = appId;
+        this.owner = owner;
+    }
+}
