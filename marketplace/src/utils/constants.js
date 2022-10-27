389f9d4cdf95fdbc09dd94b3bc2e8f4f21734f41
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-27 13:45:19 -0400

    Added libraries for algosdk and the my algo connect wallet.

---
+++ marketplace/src/utils/constants.js
@@ -0,0 +1,6 @@
+// here we define config for connection to algo network
+
+// import algosdk we need to connect with blockchain
+import algosdk from "algosdk";
+// import wallet library
+import MyAlgoConnect from "@randlabs/myalgo-connect";
