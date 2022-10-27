0c2cee2643567ab54194d64ee14d4d1aebba7b88
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-27 14:39:01 -0400

    Completed imports for marketplace front end file.

--- marketplace/src/utils/marketplace.js
+++ marketplace/src/utils/marketplace.js
@@ -10,4 +10,10 @@ import {
     numGlobalInts,
     numLocalBytes,
     numLocalInts
-}
+} from "./constants"
+
+import approvalProgram from "!!raw-loader!../contracts/marketplace_approval.teal"
+import clearProgram from "!!raw-loader!../contracts/marketplace_clear.teal"
+import { base64ToUTF8String, utf8ToBase64String } from "./conversions"
+
+
